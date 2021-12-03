329d12ca08d3f943a3adb79190c54fa22324bbcf
Szymon Łukasik <sl428760@students.mimuw.edu.pl>
2021-12-03 04:04:07 +0100

    Modified treasure.h

--- treasure.h
+++ treasure.h
@@ -6,30 +6,26 @@
 template <std::integral ValueType, bool IsTrapped>
 class Treasure {
 public:
-    static constexpr bool isTrapped = IsTrapped; 
+    static constexpr bool isTrapped = IsTrapped;
+
     constexpr Treasure(ValueType value) : value(value) {};
+
     constexpr ValueType evaluate() const {return value;};
+
     constexpr ValueType getLoot() {
         ValueType temp = value;
         value = 0;
         return temp;
     } 
+
 private:
     ValueType value;
 };
 
 template <typename ValueType>
-class SafeTreasure : Treasure<ValueType, false> {
-public:
-    constexpr
-    SafeTreasure(ValueType value) : Treasure<ValueType, false>(value) {};
-};
+using SafeTreasure = Treasure<ValueType, false>;
 
 template <typename ValueType>
-class TrappedTreasure : Treasure<ValueType, true> {
-public:
-    constexpr
-    TrappedTreasure(ValueType value) : Treasure<ValueType, true>(value) {};
-};
+using TrappedTreasure = Treasure<ValueType, true>;
 
-#endif
+#endif //__TREASURE_H
